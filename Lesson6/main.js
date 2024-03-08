aa58004c1dbe36028ecad54bddafdd1ab50f272e
DFurtune <d.furtune18@gmail.com>
2024-03-08 13:05:50 +0200

    Add Lesson 6 with Homework 6

---
+++ Lesson6/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// let zero = new Number(0);
+let zero = 0;
+// console.log(Boolean(zero));
+
+let str = "Some string";
+let str2 = "Another string";
+
+// str = str.charAt(6);
+// str = str.at(6);
+// console.log(str.endsWith('i'));
+// console.log(str.includes('Some'));
+console.log(str.indexOf("string"));
+
+let num = 10;
+
+// console.log(Number.isFinite(num));
+console.log(Number.isInteger(num));
