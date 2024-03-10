3a6e961233d58064e6f755c8bb2b11c9fc16499a
DFurtune <d.furtune18@gmail.com>
2024-03-10 12:37:21 +0200

    change Homework 6

--- Lesson6/HW6/main.js
+++ Lesson6/HW6/main.js
@@ -1,75 +1,77 @@
-'use strict'
+"use strict";
 //Level1
 //Task1-1
 function capitalize(str) {
-    let firstLetterInUpperCase = str.slice(0, 1).toUpperCase()
-    let stringInLowerCase = str.slice(1).toLowerCase()
-    return console.log(firstLetterInUpperCase + stringInLowerCase)
+  let firstLetterInUpperCase = str.slice(0, 1).toUpperCase();
+  let stringInLowerCase = str.slice(1).toLowerCase();
+  return firstLetterInUpperCase + stringInLowerCase;
 }
 
 //Task1-2
 function reverseString(str) {
-    let newString = ''
+  let newString = "";
 
-    for (let i = str.length - 1; i >= 0; i--) {
-        newString += str[i]
-    }
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
 
-    return console.log(newString)
+  return newString;
 }
 
 //Task1-3
 function convertToBinary(num) {
-    return console.log(num.toString(2))
+  return num.toString(2);
 }
 
 //Task1-4
 function generateRandomFloat(n) {
-    let randomNumber = Math.random() * 100
-    let fixedRandomNumber = randomNumber.toFixed(n)
-    return console.log(fixedRandomNumber)
+  let randomNumber = Math.random() * 100;
+  let fixedRandomNumber = randomNumber.toFixed(n);
+  return fixedRandomNumber;
 }
-
 
 //Level2
 //Task2
-
 function isPalindrom(str) {
-    let stringLowerCase = str.toLowerCase()
-    let trimString = stringLowerCase.trim()
-    return (trimString[0] === trimString[trimString.length - 1])
-}
+  let j = str.length - 1;
 
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] != str[j]) {
+      return false;
+    }
+    j--;
+  }
+  return true;
+}
 
 //Task3
 function sumOfNumbers(number) {
-    let string = number.toString()
-    let sum = 0
+  let string = number.toString();
+  let sum = 0;
 
-    for (let i = 0; i < string.length; i++) {
-        sum += Number(string[i])
-    }
-    console.log(sum)
+  for (let i = 0; i < string.length; i++) {
+    sum += Number(string[i]);
+  }
+  return sum;
 }
-
 
 //Level3
 //Task4
 function generateNumberInLimit(start, end) {
-    let rangeNumber = Math.floor((Math.random() * (end - start)) + start)
-    return console.log(rangeNumber)
+  let rangeNumber = Math.floor(Math.random() * (end - start) + start);
+  return rangeNumber;
 }
 
 //Task5
 function toCamelCase(str) {
-    let splitStr = str.split('_')
-    let result = ''
-    for (let i = 0; i < splitStr.length; i++) {
-        if (i === 0) {
-            result += splitStr[i][0].toLowerCase() + splitStr[i].slice(1)
-        } else {
-            result += splitStr[i][0].toUpperCase() + splitStr[i].slice(1)
-        }
+  let splitStr = str.split("_");
+  let result = "";
+  for (let i = 0; i < splitStr.length; i++) {
+    if (i === 0) {
+      result += splitStr[i][0].toLowerCase() + splitStr[i].slice(1);
+    } else {
+      result += splitStr[i][0].toUpperCase() + splitStr[i].slice(1);
     }
-    return result
+  }
+  return result;
 }
